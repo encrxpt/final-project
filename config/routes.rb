ae07257913141de9e0462a44de92664644b2c5e7
Edmon <encrxpt@gmail.com>
2019-11-19 10:46:38 -0600

    Generated the about controller with the index view as well as added the view as a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'about/index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resource :about
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
