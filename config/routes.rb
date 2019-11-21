7891416c041f9c5c6a270c035ac2fc0aea615968
Edmon <encrxpt@gmail.com>
2019-11-21 10:39:10 -0600

    Recreated the Product controller to Products controller as well as fixed routing within the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
 
-   get 'product/index'
-   get 'product/show'
-  # get 'about/index'
 
 
-  # resource :product, only: %i[index show]
+
+
+  get 'contacts/index'
   resource :about
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-
+  resources :products, only: %i[index show]
 
 
   root 'about#index'
+
+   # get 'customer/show'
+  # get 'category/show'
+  #  get 'product/index'
+  #  get 'product/show'
+  # get 'about/index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
