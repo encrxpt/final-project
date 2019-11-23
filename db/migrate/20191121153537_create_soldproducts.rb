021983fe8aea5261f4c58b94a7c08ad43d111bd0
Edmon <encrxpt@gmail.com>
2019-11-23 11:57:31 -0600

    Created a Contact us controller and added it as a resource to Active Admin. Also created a partial to render within the Contact index view

---
+++ db/migrate/20191121153537_create_soldproducts.rb
@@ -0,0 +1,10 @@
+class CreateSoldproducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :soldproducts do |t|
+      t.decimal :price
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
