00258d01c83986ba13bf048fc83be01e2b002107
Edmon <encrxpt@gmail.com>
2019-11-23 11:56:25 -0600

    Added navigation to see products by category, as well as being able to search using a keyword by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,19 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    @category_id = params[:product][:category_id]
+
+    @product_name = params[:product_name]
+
+    if params[:product][:category_id].present?
+        @products = Product.where("category_id = ?", @category_id)
+    end
+
+    if params[:product][:category_id].present? && params[:product_name]
+      @products = Product.where("category_id = ? and name like ?", @category_id, "%#{@product_name.downcase}%")
+    end
+
+  end
 end
